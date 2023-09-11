657a637ebf6fc981c3587889cd360f7716eac66c
carshyy <cashyyjane@gmail.com>
2023-09-11 15:01:16 -0700

    prints numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - initializes entry point
+ * declration : prints out single digits
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
