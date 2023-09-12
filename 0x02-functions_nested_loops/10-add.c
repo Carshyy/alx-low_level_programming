ada3260a85e07b4796889620e25a6ecb8169d6ac
carshyy <cashyyjane@gmail.com>
2023-09-12 06:48:56 -0700

    adds two intagers and returns result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add two values
+ * @a: integer
+ * @b: integer
+ * declaration : add two variable values
+ *
+ * Return:integer
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
