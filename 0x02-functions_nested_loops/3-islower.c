ba5db7c151688ab9032b8204cfd247c5ce931675
carshyy <cashyyjane@gmail.com>
2023-09-12 03:28:12 -0700

    checks for lowercase characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,14 +9,11 @@
  * Return: int
  */
 
-int _islower(int c)
-{
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+int _islower(int c) {
+    if (c >= 'a' && c <= 'z') {
+        return 1;  // c is lowercase
+    } else {
+        return 0;  // c is not lowercase
+    }
 }
+
