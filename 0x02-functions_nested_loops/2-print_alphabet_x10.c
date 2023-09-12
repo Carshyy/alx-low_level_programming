5f36a1de18d831c2b46b4f61d22fd43928a8e57c
carshyy <cashyyjane@gmail.com>
2023-09-12 03:00:33 -0700

    prints 10 times the alphabet in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -17,8 +17,8 @@ void print_alphabet_x10(void)
 	{
 		for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 		{
-			_putchar(alphabets);
+			putchar(alphabets);
 		}
-		_putchar('\n');
+	         putchar('\n');
 	}
 }
