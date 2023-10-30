b28f9eec0dd8a7ccbc4d50b077a816bff0c4c464
carshyy <cashyyjane@gmail.com>
2023-10-30 06:06:31 -0700

    prints a string

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,2 +1,14 @@
-_put; char(*s); = (sad)
+#include <unistd.h>
 
+/**
+ * _putchar - write the character c to stdout
+ * @c: the character to print out
+ *
+ * Return: 1 on success
+ * On error, -1 is returned, errno set approprately
+ */
+
+int _putchar(char c)
+{
+	return (write (1, &c, 1));
+}
